9ffcf7fc82df2aeb4dc8f039c27378a8c4629c09
OfficialChrisHusky <81988470+OfficialChrisHusky@users.noreply.github.com>
2023-04-25 19:07:51 +0200

    Updated Copper-Math

--- Editor Projects/DevProject/Project.cu
+++ Editor Projects/DevProject/Project.cu
@@ -2,8 +2,8 @@ Name: DevProject
 Last Scene: Scenes\Physics.copper
 Gizmo: 0
 Scene Camera:
-  Position: [-3.47488499, -0.940357089, 13.5478468]
-  Rotation: [44.886734, 404.537598, 0]
+  Position: [-3.54542661, 0.934110165, 12.2100897]
+  Rotation: [37.2072563, 430.605103, 0]
   Fov: 90
   Near Plane: 0.100000001
   Far Plane: 1000

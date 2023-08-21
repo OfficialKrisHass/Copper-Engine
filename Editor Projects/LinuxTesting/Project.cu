f6bc43efdade6f5f868218dcc9f5ac2c93b4beff
OfficialChrisHusky <chrishuskywolf@gmail.com>
2023-08-21 10:34:08 +0200

    Updated CMath

--- Editor Projects/LinuxTesting/Project.cu
+++ Editor Projects/LinuxTesting/Project.cu
@@ -2,8 +2,8 @@ Name: LinuxTesting
 Last Scene: Scenes/EmptyTemplate.copper
 Gizmo: 0
 Scene Camera:
-  Position: [-3.74605584, -15.9622726, -1.09134412]
-  Rotation: [0.67666918, -0.105335243, -0.720041513, -0.1120869]
+  Position: [-2.16180658, -15.9646444, -2.99900317]
+  Rotation: [0.255863607, -0.039579805, -0.954549015, -0.147660181]
   Fov: 90
   Near Plane: 0.100000001
   Far Plane: 1000

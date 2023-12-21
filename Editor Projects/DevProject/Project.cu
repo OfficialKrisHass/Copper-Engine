d75aa307f48f8487c48955ff37b51bc5a1eb72b8
Chris Husky <81988470+OfficialChrisHusky@users.noreply.github.com>
2023-12-22 00:42:18 +0100

    Fixed a small Camera bug

--- Editor Projects/DevProject/Project.cu
+++ Editor Projects/DevProject/Project.cu
@@ -1,8 +1,8 @@
 Name: DevProject
 Last Scene: Scenes\EmptyTemplate.copper
-Gizmo: 2
+Gizmo: 0
 Scene Camera:
-  Position: [1.64268136, 2.31323171, -6.24813366]
-  Rotation: [-0.13301079, 0.0133525552, 0.986068487, 0.0989884734]
+  Position: [5.83663654, 2.59613824, -2.13091254]
+  Rotation: [-0.697022855, 0.125680909, -0.694745183, -0.125270218]
   Speed: 0.0299999993
   Sensitivity: 110
--- premake5.lua
+++ premake5.lua
@@ -95,7 +95,7 @@ project "Copper-Engine"
         "VERSION_MAJOR=0",
         "VERSION_MINOR=2",
         "VERSION_PATCH=2",
-        "VERSION_TWEAK=15",
+        "VERSION_TWEAK=16",
         
         "SCENE_VERSION=0",
 

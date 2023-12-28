0fcdee2349343a1334fd022bb19f57787a5b2045
Chris Husky <81988470+OfficialChrisHusky@users.noreply.github.com>
2023-12-28 19:54:44 +0100

    also forgot to add the new source files to CMake :skull:

--- premake5.lua
+++ premake5.lua
@@ -95,7 +95,7 @@ project "Copper-Engine"
         "VERSION_MAJOR=0",
         "VERSION_MINOR=2",
         "VERSION_PATCH=2",
-        "VERSION_TWEAK=40",
+        "VERSION_TWEAK=41",
         
         "SCENE_VERSION=0",
 

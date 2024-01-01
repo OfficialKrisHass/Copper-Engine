f5742ba73151df7e5086f143d71d03c08b01af7c
Chris Husky <81988470+OfficialChrisHusky@users.noreply.github.com>
2024-01-02 00:09:23 +0100

    Forgot to update the version AGAIN

--- premake5.lua
+++ premake5.lua
@@ -95,7 +95,7 @@ project "Copper-Engine"
         "VERSION_MAJOR=0",
         "VERSION_MINOR=2",
         "VERSION_PATCH=2",
-        "VERSION_TWEAK=58",
+        "VERSION_TWEAK=60",
         
         "SCENE_VERSION=0",
 

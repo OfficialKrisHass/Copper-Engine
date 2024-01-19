f821e0babd7945584168ee66b7316f6bda5803e1
Chris Husky <81988470+OfficialChrisHusky@users.noreply.github.com>
2024-01-19 20:51:50 +0100

    Simple code style rules

---
+++ Code Style/Example.cpp
@@ -0,0 +1,16 @@
+#include "Example.h" // S2
+
+#include "Engine/Core/Core.h" // S3
+#include "Engine/Core/Args.h"
+
+#define MAX_ENTITIES 32
+
+namespace Copper {
+
+    static void TestSomeStuff() {
+
+        return;
+
+    }
+
+}

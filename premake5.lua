5cfe35f1ff0f4544c0f74b220e6d8142a77fb9ef
Chris Husky <81988470+OfficialKrisHass@users.noreply.github.com>
2024-02-10 16:37:44 +0100

    Forgot to add new files to CMakeLists

--- premake5.lua
+++ premake5.lua
@@ -97,7 +97,7 @@ project "Copper-Engine"
         "VERSION_MAJOR=0",
         "VERSION_MINOR=3",
         "VERSION_PATCH=0",
-        "VERSION_TWEAK=40",
+        "VERSION_TWEAK=41",
         
         "SCENE_VERSION=0",
 

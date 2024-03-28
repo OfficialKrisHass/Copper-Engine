228e9cbbf4a0cc20c96373bcace455e2d03e6d13
OfficialKrisHass <chrishuskywolf@gmail.com>
2024-03-28 16:33:36 +0100

    Removed precompiled headers due to issues

--- Copper-Engine/src/Engine/Core/UUID.h
+++ Copper-Engine/src/Engine/Core/UUID.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <uuid.h>
+#include <uuid_v4.h>
+#include <endianness.h>
 
 namespace Copper {
 
--- premake5.lua
+++ premake5.lua
@@ -98,7 +98,7 @@ project "Copper-Engine"
         "VERSION_MAJOR=0",
         "VERSION_MINOR=3",
         "VERSION_PATCH=0",
-        "VERSION_TWEAK=96",
+        "VERSION_TWEAK=97",
         
         "SCENE_VERSION=0",
 

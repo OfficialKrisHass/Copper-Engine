caae2cf35e1cef3f4905d945f482e72f4727648b
Chris Hass <81988470+OfficialKrisHass@users.noreply.github.com>
2024-03-30 15:06:18 +0100

    Removed some files and folders

--- Run.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-cd Build/linux-x86_64-Release/Copper-Editor
-./Copper-Editor Testuwu yooooo lmaoimagine
--- RunGDB.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-cd Build/linux-x86_64-Debug/Copper-Editor
-gdb Copper-Editor
